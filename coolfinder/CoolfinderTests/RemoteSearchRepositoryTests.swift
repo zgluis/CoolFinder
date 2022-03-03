1f2deef290b5019088e7c9e4681f19df0a297926
zgluis <l.zapatagt@gmail.com>
2022-03-03 17:43:14 -0300

    Initial core framework project commit

---
+++ coolfinder/CoolfinderTests/RemoteSearchRepositoryTests.swift
@@ -0,0 +1,31 @@
+//
+//  RemoteSearchRepositoryTests.swift
+//  CoolfinderTests
+//
+//  Created by Luis Zapata on 03-03-22.
+//
+
+import XCTest
+
+class RemoteSearchRepositoryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(1==1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
