93742cd97c93919debdd3a61799202aafa472081
zgluis <l.zapatagt@gmail.com>
2022-03-07 17:57:50 -0300

    Add suggestion labels

--- coolfinder/CoolFinderiOS/SearchView/SearchView.swift
+++ coolfinder/CoolFinderiOS/SearchView/SearchView.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 protocol SearchViewDelegate: AnyObject {
-    
 }
 
 final public class SearchView: UIView {
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hey! What are you up to?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = .zero
+        return label
+    }()
+    
+    lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Looking for a Motorola maybe?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = .zero
+        return label
+    }()
     
     var delegate: SearchViewDelegate?
     
@@ -30,6 +46,18 @@ final public class SearchView: UIView {
     }
     
     func configureView() {
+        addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 0),
+            titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 0),
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0)
+        ])
         
+        addSubview(subTitleLabel)
+        NSLayoutConstraint.activate([
+            subTitleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 0),
+            subTitleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 0),
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0)
+        ])
     }
 }
