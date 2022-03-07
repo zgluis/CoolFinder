ed4daf37d7bb05e731b2a30a2689710ca198a334
zgluis <l.zapatagt@gmail.com>
2022-03-06 21:18:56 -0300

    Add CoolFinderApp

---
+++ coolfinder/CoolFinderApp/CoolFinderApp/CoolFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolFinderAppApp.swift
+//  CoolFinderApp
+//
+//  Created by Luis Zapata on 06-03-22.
+//
+
+import SwiftUI
+
+@main
+struct CoolFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
