bb3650c0e1da7493c1b9468d9a56427345e2f9b6
zgluis <l.zapatagt@gmail.com>
2022-03-07 13:33:25 -0300

    Customize navigation bar according to design

--- coolfinder/CoolFinderiOS/SearchViewController.swift
+++ coolfinder/CoolFinderiOS/SearchViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import SwiftUI
 
 final public class SearchView: UIView {
 }
 
 final public class SearchViewController: UIViewController {
+    private let viewTitle = "CoolFinder"
     lazy var baseView: SearchView = {
         let baseView = SearchView()
+        baseView.backgroundColor = .green
         return baseView
     }()
     
@@ -22,6 +25,7 @@ final public class SearchViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        setUpNavigationController(title: viewTitle)
     }
     
     public func didTapSuggestionView() {
@@ -31,4 +35,33 @@ final public class SearchViewController: UIViewController {
     public func didTapReturnKey() {
         self.navigationController?.pushViewController(SearchResultViewController(), animated: true)
     }
+    
+    private func setUpNavigationController(title: String) {
+        self.title = title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        let navBarHexColor: Int = 0xf6f6f6
+        appearance.backgroundColor = UIColor(hex: navBarHexColor)
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance =
+        navigationController?.navigationBar.standardAppearance
+    }
+}
+
+extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
+
+extension Color {
+    public init(hex: Int) {
+        self.init(UIColor(hex: hex))
+   }
 }
